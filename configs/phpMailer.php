12cfde21151b45dbe899224b46908deaf80a6967
Lê Văn Trung <69235532+sevenghost-2001@users.noreply.github.com>
2024-07-27 22:57:35 +0700

    push
    +

---
+++ configs/phpMailer.php
@@ -0,0 +1,7 @@
+<?php
+$config['phpMailer'] = [
+    'smtpHost' => 'smtp.gmail.com',
+    'smtpUsername' => 'truongdat2325@gmail.com',
+    'smtpPassword' => 'houyhrzynyhjfjkz',
+    'smtpPort' => '587',
+];
